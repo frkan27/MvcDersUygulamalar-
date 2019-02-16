90c0dab680c5ea72720b8e5a9fae85bdceec0960
Furkan Kılıç <furkankilicytu@gmail.com>
2019-02-16 23:48:40 +0300

    Bundle config ok

--- Admin.Web.UI/Global.asax.cs
+++ Admin.Web.UI/Global.asax.cs
@@ -1,12 +1,14 @@
 ﻿using Admin.BLL.Identity;
 using Admin.Models.Enums;
 using Admin.Models.IdentityModels;
+using Admin.Web.UI.App_Start;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Admin.Web.UI
@@ -17,6 +19,9 @@ namespace Admin.Web.UI
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
             var roller = Enum.GetNames(typeof(IdentityRoles));
           //  var roller = new string[] { "Admin", "User" };//, koyup yeni rol ekleyebiliriz.
             var roleManager = MembershipTools.NewRoleManager();
