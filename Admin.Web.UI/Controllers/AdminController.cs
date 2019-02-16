c42fb49e6372360a221ad6171b8c613248c648c6
Furkan Kılıç <furkankilicytu@gmail.com>
2019-02-16 23:14:48 +0300

    parolasıfırlama ok

--- Admin.Web.UI/Controllers/AdminController.cs
+++ Admin.Web.UI/Controllers/AdminController.cs
@@ -1,18 +1,123 @@
-﻿using System;
+﻿using Admin.BLL.Helpers;
+using Admin.BLL.Services.Senders;
+using Admin.Models.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using static Admin.BLL.Identity.MembershipTools;
 
 namespace Admin.Web.UI.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     public class AdminController : BaseController
     {
         // GET: Admin
         public ActionResult Index()
         {
-            return View();
+            var data = NewUserStore().Users.ToList();//Kullanıcıları listele
+            return View(data);
+        }
+        [HttpPost]
+
+        public async Task<JsonResult> SendCode(string id)
+        {
+            try
+            {
+                var userStore = NewUserStore();
+                var user = await userStore.FindByIdAsync(id);
+                if (user == null)
+                {
+                    return Json(new ResponseData()
+                    {
+                        message = "Kullanıcı bulunamnadu",
+                        success = false
+                    });
+                }
+                if (user.EmailConfirmed)
+                    return Json(new ResponseData()
+                    {
+                        message = "Kullanıcı zaten e postasını onaylamıs",
+                        success = false
+                    });
+
+                user.ActivationCode = StringHelpers.GetCode();
+                await userStore.UpdateAsync(user);
+                userStore.Context.SaveChanges();
+                string SiteUrl = Request.Url.Scheme + System.Uri.SchemeDelimiter + Request.Url.Host +
+                                (Request.Url.IsDefaultPort ? "" : ":" + Request.Url.Port);
+
+                var emailService = new EMailService();
+                var body = $"Merhaba <b>{user.Name} {user.Surname}</b><br>Hesabınızı aktif etmek için aşadıdaki linke tıklayınız<br> <a href='{SiteUrl}/account/activation?code={user.ActivationCode}' >Aktivasyon Linki </a> ";
+                await emailService.SendAsync(new IdentityMessage()
+                {
+                    Body = body,
+                    Subject = "Sitemize hoşgeldiniz"
+                }, user.Email);
+                return Json(new ResponseData()
+                {
+                    message = "Kullanıcıya yeni aktivasyon maili gönderildi",
+                    success = false
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new ResponseData()
+                {
+                    message = $"Bir hata oluştu: {ex.Message}",
+                    success = false
+                });
+            }
+
+
+
+        }
+        
+        [HttpPost]
+
+        public async Task<JsonResult> SendPassword(string id)
+        {
+            try
+            {
+                var userStore = NewUserStore();
+                var user = await userStore.FindByIdAsync(id);
+                if (user == null)
+                {
+                    return Json(new ResponseData()
+                    {
+                        message = "Kullanıcı bulunamadı",
+                        success = false
+                    });
+                }
+
+                var newPassword = StringHelpers.GetCode().Substring(0, 6);
+                await userStore.SetPasswordHashAsync(user, NewUserManager().PasswordHasher.HashPassword(newPassword));
+                await userStore.UpdateAsync(user);
+                userStore.Context.SaveChanges();
+
+                string SiteUrl = Request.Url.Scheme + System.Uri.SchemeDelimiter + Request.Url.Host +
+                                                 (Request.Url.IsDefaultPort ? "" : ":" + Request.Url.Port);
+                var emailService = new EMailService();
+                var body = $"Merhaba <b>{user.Name} {user.Surname}</b><br>Hesabınızın parolası sıfırlanmıştır<br> Yeni parolanız: <b>{newPassword}</b> <p>Yukarıdaki parolayı kullanarak sistemize giriş yapabilirsiniz.</p>";
+                emailService.Send(new IdentityMessage() { Body = body, Subject = $"{user.UserName} Şifre Kurtarma" }, user.Email);
+
+                return Json(new ResponseData()
+                {
+                    message = "Şifre sıfırlama maili gönderilmiştir",
+                    success = true
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new ResponseData()
+                {
+                    message = $"Bir hata oluştu: {ex.Message}",
+                    success = false
+                });
+            }
         }
     }
 }
