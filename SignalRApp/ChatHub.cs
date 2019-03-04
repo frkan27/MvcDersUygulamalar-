a266f7956c9406e764acc258be1430e0d9a61f66
Furkan Kılıç <furkankilicytu@gmail.com>
2019-03-04 14:39:08 +0300

    chathub-ındex eklencekler var

--- SignalRApp/ChatHub.cs
+++ SignalRApp/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 
@@ -8,9 +9,31 @@ namespace SignalRApp
 {
     public class ChatHub : Hub
     {
+        //eklencek
        public void HerkeseGonder(string gonderen,string mesaj)
         {
             Clients.All.herkeseGonder(gonderen, mesaj,$"{DateTime.Now:g}");
+        }
+
+        public void OzelMesaj(string gonderenId,string AliciId,string mesaj)
+        {
+            Clients.User(AliciId).mesajGeldi(gonderenId, mesaj);
+        }
+
+        public void Login(string kullaniciAdi,string id)
+        {
+           // eklencek
+            Clients.User(id).getId(id);
+        }
+        public override Task OnConnected()
+        {
+           // UserList.Add(new )
+            return base.OnConnected();
+        }
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            //eklencek
+            return base.OnDisconnected(stopCalled);
         }
     }
 }
