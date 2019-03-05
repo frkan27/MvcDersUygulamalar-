a798d27350c6f4b268e19fbe2b7ce4cbf2cc2016
Furkan Kılıç <furkankilicytu@gmail.com>
2019-03-05 09:34:13 +0300

    devexpress and angularjs eklendi.

---
+++ DeveXOdata/Scripts/app.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="angular.js" />
+
+
+var app=angular.module("myApp",["dx"]);
+
+app.controller("testCtrl",function($scope){
+$scope.dataGridOptions={
+dataSource:customers,
+}
+
+});
