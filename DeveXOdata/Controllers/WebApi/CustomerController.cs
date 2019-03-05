318d04d225fcf4d8c87f0ef9c17657d1312deb76
Furkan Kılıç <furkankilicytu@gmail.com>
2019-03-05 12:31:22 +0300

    datagrid ok.

---
+++ DeveXOdata/Controllers/WebApi/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using DeveXOdata.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace DeveXOdata.Controllers.WebApi
+{
+    public class CustomerController : ApiController
+    {
+        // GET: Customer
+       public IHttpActionResult GetAll()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            return Ok(new
+            {
+                success = true,
+                data = db.Customers.ToList()
+            });
+        }
+    }
+}
--- DeveXOdata/Scripts/app.js
+++ DeveXOdata/Scripts/app.js
@@ -1,18 +1,108 @@
 ﻿/// <reference path="angular.js" />
 
+var app = angular.module("myApp", ["dx"]);
+var host = 'http://localhost:55599/';
+app.controller("testCtrl", function ($scope) {
 
-var app=angular.module("myApp",["dx"]);
+    $scope.dataGridOptions = {
+        dataSource: customers,
+        columns: ["CompanyName", "City", "State", "Phone", "Fax"],
+        showBorders: true,
+        searchPanel: {
+            visible: true,
+            width: 240,
+            placeholder: "Ara..."
+        }
+    };
 
-app.controller("testCtrl",function($scope){
-$scope.dataGridOptions={
-dataSource:customers,
-columns:["CompanyName","City","State","Phone","Fax"]
-showBorders:true,
-searchPanel:{
-visible:true,
-width:240]
+});
+
+app.controller("customerCtrl", function ($scope, $http) {
+    $scope.data = null;
+    function init() {
+        $http({
+            url: host + 'api/customer/getall',
+            method: 'GET'
+        }).then(function (ev) {
+            if (ev.data.success) {
+                $scope.data = ev.data.data;
+                loadGrid();
+            }
+        });
+    }//DEVEXPRESS.COM DA VAR ÖRNEKLER...syncfusionda örnekler var kullanabilirsin ama biraz zor...
+
+    function loadGrid() {
+        $scope.dataGridOptions = {
+            dataSource: $scope.data,
+            selection:{
+            mode:"multiple"//checkbox gelsin diye yaptık.  mode:single olsaydı tek seçim checkbox gelmezdi.
+             },
+            onSelectionChanged:function(selected){//seçtiğim kişiyi selected a atıyoruz.
+            $scope.selected=selected.selectedRowsData;
+             },
+             "export":{//sağ üste export ekledik.export all data ile excell oluşturdu...layout a script ekledik.
+              enabled:true,
+             fileName:"customers"+parseInt(Math.random()*100000),
+             allowExportSelectedData:true
+             },
+            columnChooser:{//sağ üstte column oluşuyor...
+             enabled:true,
+             allowSearch:true
+},
+            groupPanel:{//isme göre gruplamada kullanıyoruz...
+visible:true
+},
+filterRow:{//arama işlemi yapabiliyoruz...
+visible:true
+},
+headerFilter:{
+visible:true//header da seçtiklerimiz geliyor.TARİH varsa kullanma ya da engelle!!! sıkıntı cıkarıyor... çünkü tarihde ortak bişey yok.ortak olanları ararsın isim,adres,şehir gibi...
+},
+            columns: [{
+              dataField:"Id",
+              caption:"Müşteri no",//ıd yi çekmek için çünkü seçim yapmak istiyoruz.
+              visible:false
+               },{
+dataField:"Name",
+groupIndex: 0
+}, "Surname", "Phone",
+ {
+dataField:"Address",//denemek için addresi engelledik header filterla aranmasını...
+allowHeaderFiltering:false//normalde tarih için engelleriz aynı gelme ihtimali az olan seyler için.
+}, 
+              { 
+              dataField:"Balance",
+              caption:"Balance",
+              dataType:"number",
+              format:"#,##0.## ₺"
+            }],
+            showBorders: true,
+            paping:{pageSize:10},
+            pager:{showPageSizeSelector:true,
+            allowedPageSizes:[5,10,20],
+            showInfo:true
+            },
+            searchPanel: {
+                visible: true,
+                width: 240,
+                placeholder: "Ara..."
+            },
+summary:{//sağ altta toplam yazıyor...
+totalItems:[{
+column:"Balance",
+summaryType:"sum",
+valueFormat:"currency"
+}],
+groupItems:[{//isme göre grupluyoruz.o isimden kaç kişi varsa sayısını veriyor....
+column:"Name",
+summaryType:"count",
+displayFormat:"Toplam:{0}"
+}]
 }
+        };
+    }
 
+    init();
 });
 
 var customers=[{
